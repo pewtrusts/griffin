3a507eb1d6622cf25e45820c4e79eec58ca5f02d
John Osterman <josterman@pewtrusts.org>
2019-04-10 12:21:56 -0400

    Fixes rounding error on Math.log10

--- index.js
+++ index.js
@@ -92,14 +92,14 @@ function useNumericSymbol(config){
     var decimals = config.labelDecimals || -1,
         numericSymbols = this.series.chart.options.lang.numericSymbols,
         numericSymbolMagnitude = this.series.chart.options.lang.numericSymbolMagnitude || 1000, // 1000
-        numericSymbolPower = Math.log10(numericSymbolMagnitude), // 3
+        numericSymbolPower = Math.round(Math.log10(numericSymbolMagnitude)), // 3
         valuePower = Math.floor(Math.log10(this.point.y)),  // ie 52,000 -> 4
         value = Highcharts.numberFormat(this.point.y / 10 ** ( Math.floor(valuePower / numericSymbolPower) * numericSymbolPower), decimals), // 5.2
         symbolIndex = Math.floor(valuePower / numericSymbolPower) - 1, // 4 / 3 -> 1.3 -> 1 -> 0
         suffix = config.numberFormat === "percentage" ? '%' : '',
         prefix = config.numberFormat === "currency" ? '$' : '';
 
-    console.log(numericSymbols,numericSymbolMagnitude,numericSymbolPower,valuePower,value,symbolIndex);
+    console.log(this.point.y, numericSymbols,numericSymbolMagnitude,numericSymbolPower,valuePower,value,symbolIndex);
 
     if ( symbolIndex >= 0 && symbolIndex < numericSymbols.length ){
         return numericSymbols[symbolIndex] !== null ? prefix + value + numericSymbols[symbolIndex] + suffix : prefix + Highcharts.numberFormat(this.point.y, decimals) + suffix;
