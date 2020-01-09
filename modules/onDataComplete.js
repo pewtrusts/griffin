b4aedae4f1250bf518276d69cfaffc0678a25926
John Osterman <josterman@pewtrusts.org>
2020-01-09 11:06:23 -0500

    Removes need for endColumn; nondata columns plucked from data series array based in columnIndex data attribute

--- modules/onDataComplete.js
+++ modules/onDataComplete.js
@@ -13,6 +13,9 @@ export default function() {
     const defaultConfigs = arguments[2];
     const dataset = arguments[3];
     const Highcharts = arguments[4];
+    const nonDataColumnTypes = ['xAxisPlotBandsColumnIndex'];
+    var nondataColumns = [];
+    const nonDataIndeces = [];
 
     // if a seriesTypes attribute is provided, parse it here for use in the returnAxisConfig function
     // if none is provided, make all series the same type on basis of chart.type
@@ -104,17 +107,13 @@ export default function() {
     };
     console.log(arguments, this);
 
-    function parseNondataColumns(nondataColumns, originalArguments) {
-        console.log(nondataColumns, config.xAxisAnnotations, config.endColumn);
+    function parseNondataColumns(originalArguments) {
         nondataColumns.forEach((column, i) => {
-            console.log(config.xAxisPlotBandsColumnIndex, i + +config.endColumn + 1);
-            if (!config.xAxisPlotBands && config.xAxisPlotBandsColumnIndex == i + +config.endColumn + 1) { // i.e. endColumn = 1; index = 0;
+            if (!config.xAxisPlotBands && config.xAxisPlotBandsColumnIndex == column.originalIndex) { // i.e. plotbands not set directly (from data column) and original index equals one specified in config
                 let begin, end, plotBandInProgress = false,
                     plotBands = [];
                 column.data.forEach((d, j) => {
                     if (d[1] === 1 && !plotBandInProgress) {
-                        console.log(d);
-                        //begin = j;
                         begin = d[0];
                         plotBandInProgress = true;
                     }
@@ -137,7 +136,7 @@ export default function() {
                 console.log(originalArguments, 'plotBands!', plotBands);
             }
             // ie 2                 //0  //1
-            if (config.xAxisAnnotations == i + +config.endColumn + 1) { // i.e. endColumn = 1; index = 0;
+            if (config.xAxisAnnotations == column.originalIndex) { 
                 console.log('annotations!');
                 originalArguments[5].annotations = originalArguments[5].annotations || [];
                 originalArguments[5].annotations[0] = originalArguments[5].annotations[0] || {};
@@ -168,29 +167,40 @@ export default function() {
             }
         });
     }
+    nonDataColumnTypes.forEach(type => { // capture the indeces of all nondata columns
+        if ( config[type] !== undefined ){
+            let index = +config[type];
+            nonDataIndeces.push(index);
+        }
+    });
+    nonDataIndeces.sort(function(a,b){ // sort the indeces in descending order so that splicing one from the the original array doesn't affect the index of the next to be spliced
+        return a < b ? 1 : a > b ? -1 : 0;
+    });
+    nonDataIndeces.forEach(i => {
+        nondataColumns = nonDataIndeces.map(idx => {
+            var column = arguments[5].series.splice(idx, 1)[0];
+            column.originalIndex = idx;
+            return column;
+        }); // populate nondata columns and remove them from original array of data columns
+    });
     arguments[5].series.forEach((series, i, array) => { // eslint-disable-line no-unused-vars
         // here `series` obj has only data (array) and name (string) properties, coming from the Highcharts data module's parsing of the html tabel
-        var nondataColumns;
-        var numberOfSeries = config.endColumn || array.length;
-        console.log(config.chartType);
-        if (!config.endColumn || i < parseInt(config.endColumn)) {
-            let _series = _.defaultsDeep(series, seriesConfig, defaults(i, numberOfSeries)); // defaults return obj should include only those properties that differ from HC defaults
-            if (seriesTypes[i].match(/range/) !== null) {
-                let _data = _series.data.map(each => {
-                    var range = each[1] === null ? [null, null] : each[1].split('–').map(str => +str);
-                    return [each[0], ...range];
-                });
-                _series.data = _data;
-            }
-            if (seriesTypes[i] === 'variwide') {
-                //let _data = _series.data.map(each => {
-                console.log(_series.data);
-                //    });
-            }
-
-            series = _series;
-            console.log('series', series);
-        } else if (config.endColumn) {
+        var numberOfSeries = array.length;
+        var _series = _.defaultsDeep(series, seriesConfig, defaults(i, numberOfSeries)); // defaults return obj should include only those properties that differ from HC defaults
+        if (seriesTypes[i].match(/range/) !== null) {
+            let _data = _series.data.map(each => {
+                var range = each[1] === null ? [null, null] : each[1].split('–').map(str => +str);
+                return [each[0], ...range];
+            });
+            _series.data = _data;
+        }
+        if (seriesTypes[i] === 'variwide') {
+            //let _data = _series.data.map(each => {
+            console.log(_series.data);
+            //    });
+        }
+        series = _series;
+       /* } else if (config.endColumn) {
             console.log('nondata column', i);
             nondataColumns = arguments[5].series.splice(i); // HERE. NEED TO TAKE RETURN VALUE OF SPLICE
             // AND ITEREATE THROUGH THAT ARRAY LOOKING FOR
@@ -219,7 +229,7 @@ export default function() {
                  arguments[5].annotations = annotations;
                  console.log(arguments);
              }*/
-        }
+      /*  }
         if (config.xAxisPlotBands && (i === array.length - 1 || i === config.endColumn)) {
             let plotBands = JSON.parse(config.xAxisPlotBands);
             arguments[5].xAxis = arguments[5].xAxis || {};
@@ -243,6 +253,7 @@ export default function() {
                     }
                 };
             });
-        }
+        }*/
     });
+    parseNondataColumns(arguments);
 }
